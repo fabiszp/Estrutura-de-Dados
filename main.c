c9eafcb7551f7c87ea655cdd2e6e7e28dee5d8db
Fabiana Souza de Paula <fabisouza14112005@gmail.com>
2025-04-06 16:11:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -21,11 +21,11 @@ int main() {
         printf("1. Ordem crescente por id\n");
         printf("2. Ordem decrescente por data de ajuizamento\n");
         printf("3. Quantidade de processos por id_classe\n");
-        printf("4. Quantidade total de id_assuntos únicos\n");
+        printf("4. Quantidade total de id_assuntos unicos\n");
         printf("5. Listar processos com mais de um assunto\n");
-        printf("6. Quantidade de dias de tramitação de um processo\n");
+        printf("6. Quantidade de dias de tramitacao de um processo\n");
         printf("7. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
         getchar(); // consumir \n
 
@@ -54,7 +54,7 @@ int main() {
                 printf("Saindo...\n");
                 break;
             default:
-                printf("Opção inválida.\n");
+                printf("Opcao invalida.\n");
         }
 
     } while (opcao != 7);
